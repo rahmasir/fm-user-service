00b51f1dad37c3296ad26cc3ce63950147698c3f
EnAnsari <rahmat.ansari.dev@gmail.com>
2025-08-16 16:23:47 +0330

    create: structure of project

---
+++ src/main/java/org/rahmasir/fmuserservice/FmUserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.rahmasir.fmuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmUserServiceApplication.class, args);
+    }
+
+}
